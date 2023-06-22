ced8ffb47212cf11c209dd490fe002ca76a3fc53
Steven Hall <steven.hall@stnvideo.com>
2023-06-22 09:47:02 -0700

    Yet another change to the script because, YouTube

--- remove-youtube-shorts.user.js
+++ remove-youtube-shorts.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Remove YouTube Shorts from page
 // @namespace    https://github.com/hallzy
-// @version      0.3
+// @version      0.4
 // @description  Removes YouTube Shorts Videos from your current page.
 // @author       Steven Hall
+// @match        http://*.youtube.com/*
 // @match        https://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
@@ -13,48 +14,26 @@
     {
         const removeShorts = () =>
         {
-            const containers = [
-                'ytd-grid-video-renderer',
-                'ytd-item-section-renderer',
-                'ytd-rich-item-renderer',
-                'ytd-rich-section-renderer',
-                'ytd-video-renderer',
-
-                'ytm-grid-video-renderer',
-                'ytm-item-section-renderer',
-                'ytm-rich-item-renderer',
-                'ytm-rich-section-renderer',
-                'ytm-video-renderer',
-            ];
-
-            const elementsToRemove = [];
-            containers
+            Array
+                .from(document.querySelectorAll(`a[href^="/shorts"]`))
                 .forEach
                 (
-                    container =>
+                    a =>
                     {
-                        const shorts= Array
-                            .from(document.querySelectorAll(`${container} a[href^="/shorts"]`))
-                            .forEach
-                            (
-                                a =>
-                                {
-                                    const video = a.closest(container);
-                                    elementsToRemove.push(video);
-                                }
-                            )
-                        ;
+                        let parent = a.parentElement;
+
+                        while (parent && (!parent.tagName.startsWith('YTD-') || parent.tagName === 'YTD-THUMBNAIL'))
+                        {
+                            parent = parent.parentElement;
+                        }
+
+                        if (parent)
+                        {
+                            parent.remove();
+                        }
                     }
                 )
             ;
-
-            elementsToRemove.forEach
-            (
-                element =>
-                {
-                    element.remove();
-                }
-            );
         }
 
         const observer = new MutationObserver(removeShorts);
