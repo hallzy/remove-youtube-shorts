d4c52a273b9c02f0bb1570666a7d4e46af9d6b6f
Guillem <guille1420@gmail.com>
2023-10-31 10:44:44 +0100

    Updated query selector

--- remove-youtube-shorts.user.js
+++ remove-youtube-shorts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove YouTube Shorts from page
 // @namespace    https://github.com/hallzy
-// @version      0.4
+// @version      0.5
 // @description  Removes YouTube Shorts Videos from your current page.
 // @author       Steven Hall
 // @match        http://*.youtube.com/*
@@ -9,43 +9,20 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
-(
-    () =>
-    {
-        const removeShorts = () =>
-        {
-            Array
-                .from(document.querySelectorAll(`a[href^="/shorts"]`))
-                .forEach
-                (
-                    a =>
-                    {
-                        let parent = a.parentElement;
 
-                        while (parent && (!parent.tagName.startsWith('YTD-') || parent.tagName === 'YTD-THUMBNAIL'))
-                        {
-                            parent = parent.parentElement;
-                        }
+(() => {
+  const removeShorts = () => {
+    const shortsElements = document.querySelectorAll("[is-shorts]");
+    shortsElements.forEach((element) => {
+      element.remove();
+    });
+  };
 
-                        if (parent)
-                        {
-                            parent.remove();
-                        }
-                    }
-                )
-            ;
-        }
+  const observer = new MutationObserver(removeShorts);
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
 
-        const observer = new MutationObserver(removeShorts);
-        observer.observe
-        (
-            document,
-            {
-                childList:  true,
-                subtree:    true,
-            }
-        );
-
-        removeShorts();
-    }
-)();
+  removeShorts();
+})();
